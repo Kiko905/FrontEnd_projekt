87ee5bc9a53c38b0961d3384ceb1d3e280a81675
Kiko <kikojebest@gmail.com>
2025-01-17 15:00:23 +0100

    Fix store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,21 +1,47 @@
 import { defineStore } from 'pinia';
 
+interface User {
+  name: string;
+  age: number;
+  weight: number;
+  height: number;
+  progress: {
+    completedDays: number;
+    totalDays: number;
+  };
+}
+
 export const useUserStore = defineStore('user', {
   state: () => ({
-    name: 'Guest',
-    progress: {
-      completedDays: 0,
-      totalDays: 30,
-    },
+    users: [] as User[],
   }),
   actions: {
-    updateName(newName: string) {
-      this.name = newName;
-    },
-    incrementProgress() {
-      if (this.progress.completedDays < this.progress.totalDays) {
-        this.progress.completedDays++;
+    updateUser(index: number, newName: string, newAge: number, newWeight: number, newHeight: number) {
+      if (index >= 0 && index < this.users.length) {
+        this.users[index].name = newName;
+        this.users[index].age = newAge;
+        this.users[index].weight = newWeight;
+        this.users[index].height = newHeight;
       }
+    },
+    incrementProgress(index: number) {
+      if (index >= 0 && index < this.users.length) {
+        if (this.users[index].progress.completedDays < this.users[index].progress.totalDays) {
+          this.users[index].progress.completedDays++;
+        }
+      }
+    },
+    addUser(name: string, age: number, weight: number, height: number) {
+      this.users.push({
+        name,
+        age,
+        weight,
+        height,
+        progress: {
+          completedDays: 0,
+          totalDays: 30,
+        },
+      });
     },
   },
 });
